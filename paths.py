60af2e1aa972f465b6d826bd3011d69808b1a6fa
Simson Garfinkel <simsong@acm.org>
2023-06-02 09:32:33 +0900

    fixed HOME not being defined

--- paths.py
+++ paths.py
@@ -8,11 +8,14 @@ import functools
 
 from bottle import jinja2_view
 
+HOME = os.getenv('HOME')
+if HOME is None:
+    HOME = ''
 STATIC_DIR   = join(dirname(abspath(__file__)), 'static')
 TEMPLATE_DIR = join(dirname(relpath(__file__)), 'templates')
-DBREADER_BASH_FILE = join( os.getenv('HOME'), 'dbreader_digitalcorpora.bash')
+DBREADER_BASH_FILE = join( HOME, 'dbreader_digitalcorpora.bash')
 if not os.path.exists(DBREADER_BASH_FILE):
-    DBREADER_BASH_FILE = join( os.getenv('HOME'), 'dbreader.bash')
+    DBREADER_BASH_FILE = join( HOME, 'dbreader.bash')
 
 # Create the @view decorator to add template to the function output
 view = functools.partial(jinja2_view, template_lookup=[TEMPLATE_DIR])
