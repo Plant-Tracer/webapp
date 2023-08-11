6154e9eb494a1c1aacc62a38077bb460f7853106
Simson Garfinkel <simsong@acm.org>
2023-08-10 22:01:59 -0400

    self-test now tests mailer

--- mailer.py
+++ mailer.py
@@ -6,38 +6,39 @@ because the responder does analysis of mail files."""
 import sys
 import smtplib
 import logging
+import imaplib
+import os
+from email.parser import BytesParser
+from email import policy
+
 
 SMTP_SERVER = 'SMTP_SERVER'
 SMTP_USERNAME = 'SMTP_USERNAME'
 SMTP_PASSWORD = 'SMTP_PASSWORD'
-SMPT_PORT = 'SMTP_PORT'
+SMTP_PORT = 'SMTP_PORT'
 SMTP_PORT_DEFAULT = 587
 SMTP_DEBUG = 'SMTP_DEBUG'
 SMTP_DEBUG_DEFAULT = False
 
-def send_message(*, from_addr : str , to_addrs : [str] , msg : str, dry_run:bool = False, use_sendmail:bool = False, smtp_config:dict ):
+def send_message(*,
+                 from_addr : str ,
+                 to_addrs : [str] ,
+                 msg : str,
+                 dry_run:bool = False,
+                 smtp_config:dict ):
     # Validate types
     assert isinstance(from_addr, str)
     for to_addr in to_addrs:
         assert isinstance(to_addr, str)
-    if not use_sendmail and not smtp_config:
-        raise RuntimeError("Must either user_sendmail or provide a smtp_config")
 
     if dry_run:
         print("==== Will not send this message: ====\n{}\n====================\n".format(msg),file=sys.stderr)
         return
 
-    if use_sendmail:
-        logging.info("sending mail to %s with sendmail" ,",".join(to_addrs))
-        from subprocess import Popen,PIPE
-        p = Popen(['/usr/sbin/sendmail','-t'],stdin=PIPE)
-        p.communicate(msg.encode('utf-8'))
-        return True;
-
-    port  = smtp_config.get(SMTP_PORT, SMTP_PORT_DEFAULT)
+    port  = smtp_config.get(SMTP_PORT,  SMTP_PORT_DEFAULT)
     debug = smtp_config.get(SMTP_DEBUG, SMTP_DEBUG_DEFAULT)
 
-    with smtplib.SMTP( smtp_config[SMTP_HOST], port ) as smtp:
+    with smtplib.SMTP( smtp_config[SMTP_SERVER], port ) as smtp:
         logging.info("sending mail to %s with SMTP" ,",".join(to_addrs))
         if debug:
             smtp.set_debuglevel( 1 )
@@ -46,3 +47,50 @@ def send_message(*, from_addr : str , to_addrs : [str] , msg : str, dry_run:bool
         smtp.ehlo()
         smtp.login( smtp_config[ SMTP_USERNAME ] , smtp_config[ SMTP_PASSWORD ] )
         smtp.sendmail( from_addr, to_addrs, msg.encode('utf8'))
+
+def smtp_config_from_environ():
+    return { SMTP_SERVER : os.environ[ SMTP_SERVER ],
+             SMTP_USERNAME : os.environ[SMTP_USERNAME],
+             SMTP_PASSWORD : os.environ[SMTP_PASSWORD],
+             SMTP_PORT     : int(os.environ[SMTP_PORT])
+            }
+
+
+IMAP_SERVER   = 'IMAP_SERVER'
+IMAP_USERNAME = 'IMAP_USERNAME'
+IMAP_PASSWORD = 'IMAP_PASSWORD'
+DELETE = "<DELETE>"
+
+def imap_inbox_scan( imap_config, callback ):
+    """Call callback on each message in the imap inbox. If callback returns DELETE, then delete the message.
+    returns numbers of messages deleted.
+    """
+    deleted = 0
+    M = imaplib.IMAP4_SSL( imap_config[IMAP_SERVER])
+    M.login(imap_config[IMAP_USERNAME], imap_config[IMAP_PASSWORD] )
+    M.select()
+    typ, data = M.search(None, 'ALL')
+    for num in data[0].split():
+        typ, d2 = M.fetch(num, '(RFC822)')
+        for val in d2:
+            if type(val)==tuple:
+                (a,b) = val
+                num = a.decode('utf-8').split()[0]
+                msg = BytesParser(policy=policy.default).parsebytes(b)
+                if callback( num, msg ) is DELETE:
+                    M.store(num, '+FLAGS', '\\Deleted')
+                    deleted += 1
+    try:
+        M.expunge()
+        M.close()
+        M.logout()
+    except imaplib.IMAP4. abort as e:
+        print(e, file=sys.stderr)
+    return deleted
+
+
+def imap_config_from_environ():
+    return { IMAP_SERVER : os.environ[ IMAP_SERVER ],
+             IMAP_USERNAME : os.environ[IMAP_USERNAME],
+             IMAP_PASSWORD : os.environ[IMAP_PASSWORD]
+            }
