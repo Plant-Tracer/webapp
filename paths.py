a154a96dd6983e46600e820ae85384a26dddaba5
Simson Garfinkel <simsong@acm.org>
2023-09-03 10:27:56 -0400

    Removed PLANTTRACER_API_ENDPOINT and PLANTTRACER_HTML_ENDPOINT.
    The endpoint is only needed when sending out the email, so now it is
    just called PLANTTRACER_ENDPOINT and it travels from the environment
    variable -> bottle app -> HTML form -> POST -> bottle_app -> email.
    
    Otherwise, API is assuumed to be '/api/<foo>' whereas the html
    pages are delivered by '/<foo>' and '/static/<foo>'

--- paths.py
+++ paths.py
@@ -15,8 +15,7 @@ if HOME is None:
 STATIC_DIR   = join(dirname(abspath(__file__)), 'static')
 TEMPLATE_DIR = join(dirname(abspath(__file__)), 'templates')
 
-PLANTTRACER_API_ENDPOINT = os.environ['PLANTTRACER_API_ENDPOINT']
-PLANTTRACER_HTML_ENDPOINT = os.environ['PLANTTRACER_HTML_ENDPOINT']
+PLANTTRACER_ENDPOINT = os.environ['PLANTTRACER_ENDPOINT']
 
 
 # Add the relative template path (since jinja2 doesn't like absolute paths)
