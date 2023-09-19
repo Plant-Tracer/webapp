eb9b6faa6028370a8d7d7ce8625c5efd8773b73b
Simson Garfinkel <simsong@acm.org>
2023-09-19 00:36:24 -0400

    created logs and added active bit to api_key table

--- etc/schema.sql
+++ etc/schema.sql
@@ -50,9 +50,11 @@ CREATE TABLE `api_keys` (
   `last_used_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `use_count` int NOT NULL DEFAULT '0',
+  `active` int DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `api_keys_ibfk_1` (`user_id`),
-  CONSTRAINT `api_keys_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  CONSTRAINT `api_keys_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `active_chk` CHECK ((`active` in (0,1)))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -72,6 +74,38 @@ CREATE TABLE `courses` (
   `modified` timestamp NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `course_name` (`course_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `time_t` int NOT NULL DEFAULT (unix_timestamp()),
+  `ipaddr` varchar(39) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `apikey_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `course_id` int DEFAULT NULL,
+  `movie_id` int DEFAULT NULL,
+  `func` varchar(128) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `message` varchar(128) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `args` json DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `apikey_id` (`apikey_id`),
+  KEY `user_id` (`user_id`),
+  KEY `course_id` (`course_id`),
+  KEY `movie_id` (`movie_id`),
+  KEY `time_t` (`time_t`),
+  KEY `ipaddr` (`ipaddr`),
+  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`apikey_id`) REFERENCES `api_keys` (`id`),
+  CONSTRAINT `logs_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `logs_ibfk_3` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+  CONSTRAINT `logs_ibfk_4` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
