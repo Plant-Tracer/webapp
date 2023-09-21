d4819d4921cf99b14899082811853f5dfce30de4
Simson Garfinkel <simsong@acm.org>
2023-09-21 13:54:19 -0400

    implements logging

--- db.py
+++ db.py
@@ -9,6 +9,7 @@ import uuid
 import logging
 import json
 import sys
+import inspect
 
 from jinja2.nativetypes import NativeEnvironment
 from validate_email_address import validate_email
@@ -69,28 +70,43 @@ def get_dbwriter():
 ## Logging
 ################################################################
 
-def log(func, *, course_id=None, movie_id=None, message=None, args=None):
-    if (args is not None) and (not isinstance(args,str)):
-        args = json.dumps(args, default=str)
 
+
+"""
+    caller_frame = inspect.currentframe().f_back
+    caller_name = caller_frame.f_code.co_name
+    caller_keys, _, _, caller_args = inspect.getargvalues(caller_frame)
+    args_json = json.dumps(caller_args, default=str)
+"""
+
+def logit(*, func_name, func_args, func_return):
+    # Get the name of the caller
     user_api_key = get_user_api_key()
+    user_ipaddr  = get_user_ipaddr()
 
     if LOG_DB in logging_policy:
         dbfile.DBMySQL.csfr( get_dbwriter(),
                          """INSERT INTO logs (time_t,
-                                              apikey_id,
-                                              user_id,
-                                              ipaddr, course_id,movie_id,func,message,args)
+                                apikey_id, user_id, ipaddr,
+                                func_name, func_args, func_return)
                          VALUES (UNIX_TIMESTAMP(),
-                                (select min(id) from api_keys where api_key=%s),
-                                (select min(user_id) from api_keys where api_key=%s),
-                                %s, %s, %s, %s, %s, %s)""",
-                                (user_api_key,
-                                 user_api_key,
-                                 get_user_ipaddr(), course_id, movie_id, func, message, args))
+                                (select min(id) from api_keys where api_key=%s), (select min(user_id) from api_keys where api_key=%s), %s,
+                                 %s, %s, %s )""",
+                                (user_api_key, user_api_key, user_ipaddr,
+                                 func_name, func_args, func_return))
 
     if LOG_INFO in logging_policy:
-        logging.info("%s course_id=%s movie_id=%s message=%s args=%s",func,course_id,movie_id,message,args)
+        logging.info("%s func_name=%s func_args=%s func_return=%s",user_ipaddr, func_name, func_args, func_return)
+
+def log(func):
+    """Logging decorator."""
+    def wrapper(*args, **kwargs):
+        r = func(*args, **kwargs)
+        logit( func_name = func.__name__,
+               func_args = json.dumps( {**kwargs, **{'args':args}} ),
+               func_return = r)
+        return r
+    return wrapper
 
 def set_log_policy(v):
     logging_policy.clear()
@@ -118,6 +134,7 @@ def validate_api_key( api_key ):
     return {}
 
 ##
+@log
 def register_email(email, course_key, name):
     """Register email for a given course
     :param: email - user email
@@ -133,29 +150,27 @@ def register_email(email, course_key, name):
         raise InvalidCourse_Key( course_key )
 
     course_id = res[0][0]
-    log("register_email", course_id=course_id, args={'email':email})
     return dbfile.DBMySQL.csfr( get_dbwriter(),
                          """INSERT INTO users (email, primary_course_id, name) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE email=%s""",
                          ( email, course_id, name, email ))
 
-
+@log
 def rename_user(user_id, old_email, new_email):
     """Changes a user's email. Requires a correct old_email"""
-    log("rename_user",user_id=user_id, args={'old_email':old_email,'new_email':new_email})
     dbfile.DBMySQL.csfr( get_dbwriter(), "UPDATE users SET email=%s where id=%s AND email=%s",
                          ( old_email, user_id, new_email))
 
+@log
 def delete_user( email ):
     """Delete a user. A course cannot be deleted if it has any users. A user cannot be deleted if it has any movies.
     Also deletes the user from any courses where they may be an admin.
     """
-    log("delete_user",args={'email':email})
     dbfile.DBMySQL.csfr( get_dbwriter(), "DELETE FROM admins WHERE user_id in (select id from users where email=%s)", (email,))
     dbfile.DBMySQL.csfr( get_dbwriter(), "DELETE FROM users WHERE email=%s", (email,))
 
 
+@log
 def lookup_user( *, email ):
-    log("lookup_user", args={'email':email})
     try:
         return dbfile.DBMySQL.csfr( get_dbreader(), "select * from users where email=%s",(email,), asDicts=True)[0]
     except IndexError:
@@ -174,11 +189,11 @@ def make_new_api_key( email ):
                              """INSERT INTO api_keys (user_id, api_key) VALUES (%s,%s)""",
 
                              (user_id, api_key))
-        log("make_new_api_key", args={'user_id':user_id,'api_keys.id=':r})
         return api_key
     return None
 
 
+@log
 def delete_api_key(api_key):
     """Deletes an api_key
     :param: api_key - the api_key
@@ -186,11 +201,11 @@ def delete_api_key(api_key):
     """
     if len(api_key) < 10:
         raise InvalidAPI_Key(api_key)
-    log("delete_api_key",args={'api_key':api_key})
     return dbfile.DBMySQL.csfr( get_dbwriter(),
                                 """DELETE FROM api_keys WHERE api_key=%s""",
                                 (api_key,))
 
+@log
 def send_links( email, planttracer_endpoint ):
     """Creates a new api key and sends it to email. Won't resend if it has been sent in MIN_SEND_INTERVAL"""
     PROJECT_EMAIL = 'admin@planttracer.com'
@@ -202,9 +217,6 @@ def send_links( email, planttracer_endpoint ):
         msg_env = NativeEnvironment().from_string( f.read() )
     new_api_key = make_new_api_key( email )
 
-    log("send_links", args={'to_addrs':email,
-                            'from_addr':PROJECT_EMAIL,
-                            'planttracer_endpoint':planttracer_endpoint})
     if new_api_key:
         msg = msg_env.render( to_addrs   = ",".join([email]),
                               from_addr  = PROJECT_EMAIL,
@@ -233,21 +245,23 @@ def lookup_course( *, course_id ):
     except IndexError:
         return {}
 
+@log
 def create_course(course_key, course_name, max_enrollment, course_section=None):
     """Create a new course
     :return: course_id of the new course
     """
     ret =dbfile.DBMySQL.csfr( get_dbwriter(), "INSERT into courses (course_key, course_name, max_enrollment, course_section) values (%s,%s,%s,%s)",
                                 (course_key, course_name, max_enrollment, course_section))
-    log("create_course",args={'course_name':course_name,'max_enrollment':max_enrollment, 'course_section':course_section, 'ret':ret})
     return ret
 
+@log
 def delete_course(course_key):
     """Delete a course.
     :return: number of courses deleted.
     """
     return dbfile.DBMySQL.csfr( get_dbwriter(), "DELETE from courses where course_key=%s", (course_key,))
 
+@log
 def make_course_admin( email, *, course_key=None, course_id=None ):
     if course_id:
         dbfile.DBMySQL.csfr( get_dbwriter(), "INSERT into admins (course_id, user_id) values (%s, (select id from users where email=%s))",
@@ -256,6 +270,7 @@ def make_course_admin( email, *, course_key=None, course_id=None ):
         dbfile.DBMySQL.csfr( get_dbwriter(), "INSERT into admins (course_id, user_id) values ((select id from courses where course_key=%s), (select id from users where email=%s))",
                              (course_key, email))
 
+@log
 def remove_course_admin( email, *, course_key=None, course_id=None ):
     if course_id:
         dbfile.DBMySQL.csfr( get_dbwriter(), "DELETE FROM admins WHERE course_id=%s and user_id in (select id from users where email=%s)",
@@ -266,7 +281,7 @@ def remove_course_admin( email, *, course_key=None, course_id=None ):
                              "AND user_id IN (SELECT id FROM users WHERE email=%s)",
                              (course_key, email))
 
-
+@log
 def check_course_admin( user_id, course_id ):
     """Return True if user_id is an admin in course_id"""
     res = dbfile.DBMySQL.csfr( get_dbreader(), "SELECT * FROM admins WHERE user_id=%s AND course_id=%s LIMIT 1",
@@ -274,19 +289,23 @@ def check_course_admin( user_id, course_id ):
     return len(res)==1
 
 
+@log
 def validate_course_key( course_key ):
     res = dbfile.DBMySQL.csfr( get_dbreader(),
                               """SELECT course_key FROM courses WHERE course_key=%s LIMIT 1""", (course_key,))
     return len(res)==1 and res[0][0]==course_key
 
+@log
 def remaining_course_registrations( course_key ):
     res = dbfile.DBMySQL.csfr( get_dbreader(),
-                              """SELECT max_enrollment - (SELECT COUNT(*) FROM users WHERE primary_course_id=(SELECT id FROM courses WHERE course_key=%s))
-                                 FROM courses WHERE course_key=%s""",
+                              """SELECT max_enrollment
+                              - (SELECT COUNT(*) FROM users
+                              WHERE primary_course_id=(SELECT id FROM courses WHERE course_key=%s))
+                              FROM courses WHERE course_key=%s""",
                               ( course_key,course_key))
     try:
         return int(res[0][0])
-    except (IndexError,ValueError):
+    except (IndexError,ValueError) as e:
         return 0
 
 
@@ -296,10 +315,12 @@ def remaining_course_registrations( course_key ):
 ################################################################
 
 
+@log
 def get_movie( movie_id ):
     """Returns the movie contents. Does no checking"""
     return dbfile.DBMySQL.csfr( get_dbreader(), "SELECT movie_data from movie_data where movie_id=%s LIMIT 1",(movie_id,))[0][0]
 
+@log
 def get_movie_metadata( user_id, movie_id ):
     cmd = """SELECT * from movies WHERE
                 (user_id=%s OR
@@ -311,6 +332,7 @@ def get_movie_metadata( user_id, movie_id ):
         params.append(movie_id)
     return dbfile.DBMySQL.csfr( get_dbreader(), cmd, params, asDicts=True)
 
+@log
 def can_access_movie( user_id, movie_id ):
     """Return if the user is allowed to access the movie"""
     res = dbfile.DBMySQL.csfr(
@@ -322,16 +344,19 @@ def can_access_movie( user_id, movie_id ):
         (movie_id, user_id, user_id, user_id))
     return res[0][0]>0
 
+@log
 def purge_movie(  movie_id ):
     """Actually delete a movie and all its frames"""
     dbfile.DBMySQL.csfr( get_dbwriter(), "DELETE from movie_frames where movie_id=%s", (movie_id,))
     dbfile.DBMySQL.csfr( get_dbwriter(), "DELETE from movie_data where movie_id=%s", (movie_id,))
     dbfile.DBMySQL.csfr( get_dbwriter(), "DELETE from movies where id=%s", (movie_id,))
 
+@log
 def delete_movie( movie_id, delete=1 ):
     """Set a movie's deleted bit to be true"""
     dbfile.DBMySQL.csfr( get_dbwriter(), "UPDATE movies SET deleted=%s where id=%s", (delete, movie_id,))
 
+@log
 def create_new_movie(user_id, *, title=None, description=None, movie_data=None):
     res = dbfile.DBMySQL.csfr( get_dbreader(),"select primary_course_id from users where id=%s",(user_id,))
     if not res or len(res)!=1:
@@ -360,7 +385,6 @@ def create_new_frame( movie_id, frame_msec, frame_base64_data ):
     return frame_id
 
 
-LM_DEBUG=False
 def list_movies( user_id ):
     """Return a list of movies that the user is allowed to access.
     This should be updated so that we can request only a specific movie
@@ -375,9 +399,6 @@ def list_movies( user_id ):
                                 OR
                                 (course_id in (SELECT course_id FROM admins WHERE user_id=%s))""",
                                 (user_id, user_id, user_id), asDicts=True)
-    if LM_DEBUG:
-        for r in res:
-            logging.error("res=%s",res)
     return res
 
 # set movie metadata privileges array:
@@ -396,19 +417,19 @@ SET_MOVIE_METADATA = {
     'published':'update movies set published=%s where id=%s and (@is_admin or (@is_owner and published!=0))',
 }
 
+@log
 def set_metadata(*, user_id, set_movie_id=None, set_user_id=None, property, value):
     """We tried doing this in a single statement and it failed"""
     # First compute @is_owner
 
-    logging.warning("set_user_id=%s set_movie_id=%s property=%s value=%s",set_user_id, set_movie_id,property,value)
+    logging.info("set_user_id=%s set_movie_id=%s property=%s value=%s",set_user_id, set_movie_id,property,value)
     assert isinstance(user_id,int)
     assert isinstance(set_movie_id,int) or (set_movie_id is None)
     assert isinstance(set_user_id,int) or (set_user_id is None)
     assert isinstance(property,str)
     assert value is not None
 
-    MAPPER = {0:'FALSE',
-              1:'TRUE'}
+    MAPPER = {0:'FALSE', 1:'TRUE'}
 
     if set_movie_id:
         res = dbfile.DBMySQL.csfr( get_dbreader(), "SELECT %s in (select user_id from movies where id=%s)", (user_id, set_movie_id))
@@ -417,7 +438,6 @@ def set_metadata(*, user_id, set_movie_id=None, set_user_id=None, property, valu
         res = dbfile.DBMySQL.csfr( get_dbreader(), "SELECT %s in (select user_id from admins where course_id=(select course_id from movies where id=%s))",
                                    (user_id, set_movie_id))
         is_admin = MAPPER[res[0][0]]
-        logging.warning("user_id=%s set_movie_id=%s property=%s value=%s is_owner=%s is_admin=%s",user_id, set_movie_id, property, value, is_owner,is_admin)
 
         cmd = SET_MOVIE_METADATA[property].replace('@is_owner',is_owner).replace('@is_admin',is_admin)
         args = [ value, set_movie_id ]
