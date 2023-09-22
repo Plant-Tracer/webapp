e827b03d03b0c2990de15d072bd6b7b6d2a50802
JoAnn Juzefyk <109179653+JoAnnJuzefyk@users.noreply.github.com>
2023-09-22 12:39:23 -0400

    test for type error

--- tests/gravitropism_test.py
+++ tests/gravitropism_test.py
@@ -13,13 +13,15 @@ from gravitropism import calculate_results_gravitropism
     (0, 0, 0, 0, 2, {'distance': pytest.approx(0.0, 0.01), 'rate': pytest.approx(0.0, 0.01), 'angle': pytest.approx(0.0, 0.1)}),
     (3, 4, 0, 1, 2, {'distance': pytest.approx(4.24, 0.01), 'rate': pytest.approx(2.12, 0.01), 'angle': pytest.approx(-135.0, 0.1)})
 ])
-
-
 def test_calculate_two_points(x1, y1, x2, y2, time_elapsed, expected_results):
-    
+        
     results = calculate_results_gravitropism(x1, y1, x2, y2, time_elapsed)
     
     assert results == expected_results
     
-    
+def test_coordinates_type_error():
+    with pytest.raises(TypeError) as e:
+        calculate_results_gravitropism(1, 2, 3, 4, 0)
+        assert e.message == "All coordinates must be provided and time elapsed must be greater than zero."
+            
     
