ddb0e96763c4b68f137d357a3e740b5fd9c917eb
Simson Garfinkel <simsong@acm.org>
2023-10-15 18:58:32 -0400

    endpoint_test.py now passes

--- tests/user_test.py
+++ tests/user_test.py
@@ -12,9 +12,9 @@ import uuid
 import base64
 import time
 import bottle
+import copy
 
 from os.path import abspath, dirname
-
 
 sys.path.append(dirname(dirname(abspath(__file__))))
 
@@ -33,84 +33,92 @@ TEST_ADMIN_NAME = 'Test User Name'
 
 MOVIE_FILENAME = os.path.join(MYDIR, "data", "2019-07-31 plantmovie.mov")
 
-def movie_list(api_key):
-    """Return a list of the movies"""
-    with boddle(params={"api_key": api_key}):
-        res = bottle_app.api_list_movies()
-    assert res['error'] == False
-    return res['movies']
-
-def get_movie(api_key, movie_id):
-    """Used for testing. Just pull the specific movie"""
-    movies = movie_list(api_key)
-    for movie in movies:
-        return movie
-
-    user_id = db.validate_api_key(api_key)['user_id']
-    logging.error("api_key=%s movie_id=%s user_id=%s",
-                  api_key, movie_id, user_id)
-    logging.error("len(movies)=%s", len(movies))
-    for movie in movies:
-        logging.error("%s", str(movie))
-    dbreader = db.get_dbreader()
-    logging.error("Full database: (dbreader: %s)", dbreader)
-    for movie in dbfile.DBMySQL.csfr(dbreader, "select * from movies", (), asDicts=True):
-        logging.error("%s", str(movie))
-    raise RuntimeError(f"No movie has movie_id {movie_id}")
-
-
+# keys for scaffolding dictionary
+ADMIN_EMAIL = 'admin_email'
+ADMIN_ID = 'admin_id'
+API_KEY  = 'api_key'
+COURSE_KEY = 'course_key'
+COURSE_NAME = 'course_name'
+USER_EMAIL = 'user_email'
+USER_ID    = 'user_id'
 
 ################################################################
 
 @pytest.fixture
 def new_course():
-    """Fixture to create a new course and then delete it. New course creates a new course admin and a new user for it"""
+    """Fixture to create a new course and then delete it.
+    New course creates a new course admin and a new user for it"""
+
     course_key = str(uuid.uuid4())[0:4]
     admin_email = TEST_ADMIN_EMAIL.replace('@', '+'+str(uuid.uuid4())[0:4]+'@')
+    course_name = course_key + "course name"
 
-    ct = db.create_course(course_key=course_key, course_name=course_key + "course name", max_enrollment=MAX_ENROLLMENT)['course_id']
+    ct = db.create_course(course_key=course_key,
+                          course_name=course_name,
+                          max_enrollment=MAX_ENROLLMENT)['course_id']
+
     admin_id = db.register_email(email=admin_email, course_key=course_key, name=TEST_ADMIN_NAME)['user_id']
+
     logging.info("generated course_key=%s  admin_email=%s admin_id=%s",course_key,admin_email,admin_id)
     db.make_course_admin(email=admin_email, course_key=course_key)
-    yield (course_key,admin_email)
+
+    yield {COURSE_KEY:course_key,
+           COURSE_NAME:course_name,
+           ADMIN_EMAIL:admin_email,
+           ADMIN_ID:admin_id
+           }
     db.remove_course_admin(email=admin_email, course_key=course_key)
     db.delete_user(email=admin_email)
     ct = db.delete_course(course_key=course_key)
     assert ct == 1                # returns number of courses deleted
-
 
 @pytest.fixture
 def new_user(new_course):
     """Creates a new course and a new user and yields (USER_EMAIL, api_key)
     Then deletes them. The course gets deleted by the new_course fixture.
     """
-    (course_key, admin_email) = new_course
+    cfg = copy.copy(new_course)
 
     user_email = TEST_USER_EMAIL.replace('@', '+'+str(uuid.uuid4())[0:6]+'@')
-    user_id = db.register_email(email=user_email, course_key=course_key, name=TEST_USER_NAME)['user_id']
+    user_id = db.register_email(email=user_email,
+                                course_key=cfg[COURSE_KEY],
+                                name=TEST_USER_NAME)['user_id']
     logging.info("generated user_email=%s user_id=%s",user_email, user_id)
 
     api_key = db.make_new_api_key(email=user_email)
     assert len(api_key) > 8
-    yield (user_email, api_key)
+
+    cfg[API_KEY] = api_key
+    cfg[USER_EMAIL] = user_email
+    cfg[USER_ID] = user_id
+
+    yield cfg
     ct = db.delete_api_key(api_key)
     assert ct == 1
     db.delete_user(email=user_email)
+
+@pytest.fixture
+def api_key(new_user):
+    """Simple fixture that just returns a valid api_key"""
+    yield new_user[API_KEY]
 
 
 @pytest.fixture
 def new_movie(new_user):
     """Create a new movie_id and return it"""
-    (email, api_key) = new_user
+    cfg = copy.copy(new_user)
 
-    MOVIE_TITLE = 'test movie title ' + str(uuid.uuid4())
+    api_key = cfg[API_KEY]
+    api_key_invalid = api_key+"invalid"
+
+    movie_title = 'test movie title ' + str(uuid.uuid4())
 
     with open(MOVIE_FILENAME, "rb") as f:
         movie_base64_data = base64.b64encode(f.read())
 
    # Try to uplaod the movie with an invalid key
-    with boddle(params={"api_key": api_key+'invalid',
-                        "title": MOVIE_TITLE,
+    with boddle(params={"api_key": api_key_invalid,
+                        "title": movie_title,
                         "description": "test movie description",
                         "movie_base64_data": movie_base64_data}):
         bottle_app.expand_memfile_max()
@@ -119,14 +127,18 @@ def new_movie(new_user):
 
     # Try to uplaod the movie all at once
     with boddle(params={"api_key": api_key,
-                        "title": MOVIE_TITLE,
+                        "title": movie_title,
                         "description": "test movie description",
                         "movie_base64_data": movie_base64_data}):
         res = bottle_app.api_new_movie()
     assert res['error'] == False
     movie_id = res['movie_id']
     assert movie_id > 0
-    yield (movie_id, MOVIE_TITLE, api_key)
+
+    cfg[MOVIE_ID] = movie_id
+    cfg[MOVIE_TITLE] = movie_title
+
+    yield cfg
 
     # Delete the movie we uploaded
     with boddle(params={'api_key': api_key,
@@ -138,16 +150,27 @@ def new_movie(new_user):
     db.purge_movie(movie_id=movie_id)
 
 
+################################################################
+## fixutre tests
+################################################################
+
+
+
+
 def test_new_course(new_course):
-    (course_key, admin_email) = new_course
+    cfg = copy.copy(new_course)
+    course_key = cfg[COURSE_KEY]
+    admin_email = cfg[ADMIN_EMAIL]
     logging.info("Created course %s", course_key)
 
 def test_new_user(new_user):
-    (email, api_key) = new_user
-    logging.info("email=%s api_key=%s", email, api_key)
+    cfg = copy.copy(new_course)
+    user_email = cfg[USER_EMAIL]
+    api_key = cfg[API_KEY]
+    logging.info("user_email=%s api_key=%s", email, api_key)
 
     # Try looking up the user
-    ret1 = db.lookup_user(email=email)
+    ret1 = db.lookup_user(email=user_email)
     ret2 = db.lookup_user(user_id=ret1['user_id'], get_admin=True)
     ret3 = db.lookup_user(user_id=ret1['user_id'], get_courses=True)
 
@@ -157,7 +180,10 @@ def test_new_user(new_user):
 
 def test_movie_upload(new_movie):
     """Create a new user, upload the movie, delete the movie, and shut down"""
-    (movie_id, movie_title, api_key) = new_movie
+    cfg = copy.copy(new_movie)
+    movie_id = cfg[MOVIE_ID]
+    movie_title = cfg[MOVIE_TITLE]
+    api_key = cfg[API_KEY]
 
     # Did the movie appear in the list?
     movies = movie_list(api_key)
@@ -173,8 +199,10 @@ def test_movie_upload(new_movie):
 
 def test_movie_update_metadata(new_movie):
     """try updating the metadata, and making sure some updates fail."""
-
-    (movie_id, movie_title, api_key) = new_movie
+    cfg = copy.copy(new_movie)
+    movie_id = cfg[MOVIE_ID]
+    movie_title = cfg[MOVIE_TITLE]
+    api_key = cfg[API_KEY]
 
     # Validate the old title
     assert get_movie(api_key, movie_id)['title'] == movie_title
@@ -229,6 +257,37 @@ def test_movie_update_metadata(new_movie):
 
     # Try to publish the movie with the course admin's API key. This should work
 
+################################################################
+## support functions
+################################################################
+
+
+def movie_list(api_key):
+    """Return a list of the movies"""
+    with boddle(params={"api_key": api_key}):
+        res = bottle_app.api_list_movies()
+    assert res['error'] == False
+    return res['movies']
+
+def get_movie(api_key, movie_id):
+    """Used for testing. Just pull the specific movie"""
+    movies = movie_list(api_key)
+    for movie in movies:
+        return movie
+
+    user_id = db.validate_api_key(api_key)['user_id']
+    logging.error("api_key=%s movie_id=%s user_id=%s",
+                  api_key, movie_id, user_id)
+    logging.error("len(movies)=%s", len(movies))
+    for movie in movies:
+        logging.error("%s", str(movie))
+    dbreader = db.get_dbreader()
+    logging.error("Full database: (dbreader: %s)", dbreader)
+    for movie in dbfile.DBMySQL.csfr(dbreader, "select * from movies", (), asDicts=True):
+        logging.error("%s", str(movie))
+    raise RuntimeError(f"No movie has movie_id {movie_id}")
+
+
 def test_get_logs():
     """Incrementally test each part of the get_logs functions"""
     dbreader = db.get_dbreader()
@@ -243,9 +302,12 @@ def test_get_logs():
         db.get_logs( user_id=0, ipaddr = "", security=security)
 
 def test_course_list(new_user):
-    (user_email, api_key) = new_user
+    cfg        = copy.copy(new_movie)
+    user_email = cfg[USER_EMAIL]
+    api_key    = cfg[API_KEY]
+
     user_dict = db.validate_api_key(api_key)
-    user_id = user_dict['user_id']
+    user_id   = user_dict['user_id']
     primary_course_id = user_dict['primary_course_id']
 
     recs1 = db.list_users(user_id=user_id)
@@ -271,8 +333,11 @@ def test_course_list(new_user):
 
 def test_log_search_user(new_user):
     """Currently we just run logfile queries and count the number of results."""
-    (user_email, api_key) = new_user
-    user_id = db.validate_api_key(api_key)['user_id']
+    cfg        = copy.copy(new_movie)
+    user_email = cfg[USER_EMAIL]
+    api_key    = cfg[API_KEY]
+
+    user_id  = db.validate_api_key(api_key)['user_id']
     dbreader = db.get_dbreader()
 
     ret = db.get_logs( user_id=user_id )
@@ -299,7 +364,9 @@ def test_log_search_user(new_user):
     assert(len(db.get_logs( user_id=user_id, course_key = course_key)) > 0)
 
     # Test to make sure that the course admin gets access to this user
-    admin_id = dbfile.DBMySQL.csfr(dbreader, "select user_id from admins where course_id=%s LIMIT 1", (course_id,))[0]
+    admin_id = dbfile.DBMySQL.csfr(dbreader,
+                                   "SELECT user_id FROM admins WHERE course_id=%s LIMIT 1",
+                                   (course_id,))[0]
     assert(len(db.get_logs( user_id=admin_id, log_user_id=user_id, course_id = course_id)) > 0)
     assert(len(db.get_logs( user_id=admin_id, log_user_id=user_id, course_key = course_key)) > 0)
 
@@ -307,7 +374,11 @@ def test_log_search_user(new_user):
     assert(len(db.get_logs( user_id=user_id, ipaddr="0.0.0.0"))==0)
 
 def test_log_search_movie(new_movie):
-    (movie_id, movie_title, api_key) = new_movie
+    cfg        = copy.copy(new_movie)
+    api_key    = cfg[API_KEY]
+    movie_id   = cfg[MOVIE_ID]
+    movie_title= cfg[MOVIE_TITLE]
+
     dbreader = db.get_dbreader()
     res = dbfile.DBMySQL.csfr(dbreader, "select user_id from movies where id=%s", (movie_id,))
     user_id = res[0][0]
