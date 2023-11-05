6594ddccb05d4b0f0045661c986b4774e846ab5c
Simson Garfinkel <simsong@acm.org>
2023-11-05 08:29:23 -0500

    now loads image with here document

--- static/analyze.js
+++ static/analyze.js
@@ -92,11 +92,6 @@ class myImage extends myObject {
         this.draggable = false;
         this.ctx = null;
         this.img = new Image();
-        this.img.src = url;
-        this.draw = function (ctx) {
-            theImage.ctx = ctx;
-            ctx.drawImage(this.img, 0, 0);
-        }
 
         // When the image is loaded, draw the entire stack again.
         this.img.onload = function() {
@@ -104,6 +99,14 @@ class myImage extends myObject {
                 draw( theImage.ctx );
             }
         }
+        this.draw = function (ctx) {
+            theImage.ctx = ctx;
+            ctx.drawImage(this.img, 0, 0);
+        }
+
+        // set the URL. It loads immediately if it is a here document.
+        this.img.src = url;
+
     }
 }
 
@@ -224,7 +227,12 @@ function mouseChanged(e) {
     draw();
 }
 
+function slider_moved() {
+    draw();
+}
+
 function add_marker_clicked() {
+    add_circle( 50, 50, $('#marker-name').val());
 }
 
 function done_button_clicked() {
@@ -258,8 +266,6 @@ function analyze_movie() {
     // Say which movie it is and load the first frame
     $('#firsth2').html(`Movie ${movie_id}`);
 
-    // URL of the first frame in the movie
-    const url = `/api/get-frame?movie_id=${movie_id}&api_key=${api_key}&frame_msec=0&msec_delta=0`;
 
     // The canvas is defined int he template
     globals.c   = document.getElementById('c1'); // get the canvas
@@ -269,13 +275,27 @@ function analyze_movie() {
     while (globals.objects.length > 0){
         globals.objects.pop();
     }
+
     // Create the objects. Draw order is insertion order.
     // Image first means that the circles go on top
-    globals.objects.push( new myImage( 0, 0, url ));
+    // URL of the first frame in the movie
+
+    // old style:
+    // const url = `/api/get-frame?movie_id=${movie_id}&api_key=${api_key}&frame_msec=0&msec_delta=0`;
+    // globals.objects.push( new myImage( 0, 0, url ));
+
+    // new style:
+    $.post('/api/get-frame', {movie_id:movie_id, api_key:api_key, frame_msec:0, msec_delta:0, format:'json', analysis:true})
+        .done( function(data) {
+            data = JSON.parse(data); // convert to JSON
+            globals.objects.push( new myImage( 0, 0, data.data_url));
+            setTimeout( draw, 1000); // trigger a reload at 1 second just in case.
+        });
+
 
     // For testing, just add two circles
-    add_circle( 50, 50, "first");
-    add_circle( 25, 25, "second");
+    // add_circle( 50, 50, "first");
+    // add_circle( 25, 25, "second");
 
     // Initial drawing
     draw();
