382ea4e65ddb60aea42afd150fcb7764375ad4b8
Steve Barber <sbarber2@gmail.com>
2023-12-15 13:43:31 -0500

    Run Chrome headless with selenium. Close browser with selenium. Specify driver in pytest for pytest_selenium. Clean up comments.

--- tests/sitetitle_test.py
+++ tests/sitetitle_test.py
@@ -1,20 +1,37 @@
+"""
+Test functions to verify the title of the Plant Tracer webapp home/index page
+
+This module also serves to illustrate how to use Selenium and Pytest together.
+
+The two tests here test the same thing, but one uses the pytest-selenium
+package and the other uses the selenium package directly.
+"""
+
 import pytest
 import pytest_selenium
 
 # not needed for using pytest-selenium, just for using selenium directly
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-PLANTTRACER_TITLE = 'Plant Tracer Plant Tracer' # TODO: this is not the title we want. Should just be 'Plant Tracer'
-#PLANTTRACER_TITLE = 'Plant Tracer' # this is the desired title
+PLANTTRACER_TITLE = 'Plant Tracer Plant Tracer'
 
-# test function illustraing the use of selenium package directly
+# test function illustrating the use of selenium package directly
 def test_sitetitle_just_selenium(http_endpoint):
-    browser_driver = webdriver.Chrome() # TODO: externalize browser type, if possible
+    options = Options()
+    options.headless = True
+    browser_driver = webdriver.Chrome(options = options) # TODO: externalize browser type, if possible
     browser_driver.get(http_endpoint)
     assert browser_driver.title == PLANTTRACER_TITLE
+    browser_driver.close()
 
 # test function illustrating the use of pytest-selenium package
 # pytest-selenium itself uses the selenium package
-def test_sitetitle_pytest_selenium(http_endpoint, selenium):
+#
+# Using pytest-selenium requires the --driver command line argument to pytest
+# --driver may be specified in pytest.ini using addopts Configuration option
+#
+# TODO: test should probably be headless, or at least clean up their browser processes
+def test_sitetitle_pytest_selenium(http_endpoint, chrome_options, selenium):
     selenium.get(http_endpoint) # TODO: externalize site URL
     assert selenium.title == PLANTTRACER_TITLE
