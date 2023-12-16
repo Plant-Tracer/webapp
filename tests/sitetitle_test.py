14b57087a590a2c18a7afa093275c6383de11295
Simson Garfinkel <simsong@acm.org>
2023-12-16 00:08:56 +0000

    added more for headless

--- tests/sitetitle_test.py
+++ tests/sitetitle_test.py
@@ -20,10 +20,18 @@ PLANTTRACER_TITLE = 'Plant Tracer'
 def test_sitetitle_just_selenium(http_endpoint):
     options = Options()
     options.headless = True
+    #
+    # Following added per ChatGPT-4:
+    options.add_argument("--no-sandbox")  # This option is often necessary in Docker or CI environments
+    options.add_argument("--disable-gpu")  # This option is recommended for headless mode
+    options.add_argument("--window-size=1920,1080")  # Specify window size
+
+
     browser_driver = webdriver.Chrome(options = options) # TODO: externalize browser type, if possible
     browser_driver.get(http_endpoint)
     assert browser_driver.title == PLANTTRACER_TITLE
     browser_driver.close()
+    browser_driver.quit()
 
 # test function illustrating the use of pytest-selenium package
 # pytest-selenium itself uses the selenium package
