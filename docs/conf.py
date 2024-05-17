2f8aaa40e97bae45d63691ba9f3c827b174ce783
Simson Garfinkel <simsong@acm.org>
2024-05-17 15:17:14 -0400

    moved .md to .rst and updates files based on what is in another project

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,15 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+# Add project root to pythonpath
+import os
+import sys
+
+from os.path import dirname,abspath
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+from constants import __version__
+
 project = 'Plant Tracer Web Application'
 copyright = '2024, Simson Garfinkel, Steve Barber, JoAnn Juzefyk, Eric Brenner'
 author = 'Simson Garfinkel, Steve Barber, JoAnn Juzefyk, Eric Brenner'
@@ -14,15 +23,37 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "furo"
 html_static_path = ['_static']
+
+# autodoc
+
+autoclass_content = "both"
+autodoc_default_options = {
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+}
+autodoc_typehints_format = "short"
