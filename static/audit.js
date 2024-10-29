5423b52cba53930c96037b2c18c2fa8793db59b7
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2024-10-29 13:51:38 -0400

    Removing audit tests for future PR

--- static/audit.js
+++ static/audit.js
@@ -2,9 +2,6 @@
 /* jshint esversion: 8 */
 /* global api_key */
 
-const $ = require('jquery');
-global.$ = $;
-
 ////////////////////////////////////////////////////////////////
 // page: /audit
 // This could fill in the table with search keys; right now we just search for everything
