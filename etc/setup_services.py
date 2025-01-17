43a9725ecc32b053e7fc7efcd3ea6f73d1ac685b
Simson Garfinkel <simsong@acm.org>
2025-01-17 02:22:03 +0000

    added base to template

--- etc/setup_services.py
+++ etc/setup_services.py
@@ -15,19 +15,19 @@ def writefile(fname,name,content):
 HTTPD_TEMPLATE=readfile('planttracer-template.conf')
 SYSTEMD_TEMPLATE=readfile('planttracer-template.service')
 
-HTTPD_DIR =
-SYSTEMD_DIR = '/etc/systemd/system'
+# name,port,demo,base
+DEFS = """mv1,8000,no,/home/ec2-user/webapp
+app,8010,no,/home/ec2-user/webapp
+demo1,8020,no,/home/ec2-user/webapp
+dev-slg,8030,no,/home/ec2-user/dev-slg"""
 
 
-DEFS = """mv1,8000,no
-app,8010,no
-demo1,8020,no
-dev-slg,8030,no"""
-
-
-for def in DEFS.split('\n'):
-    for (name,port,demo) in def.split(','):
-        writefile('/etc/httpd/conf.d/planttracer-{name}.conf', name, content.format(name=name, port=port, demo=demo))
-        writefile('/etc/systemd/system/planttracer-{name}.service', name, content.format(name=name, port=port, demo=demo))
+for d in DEFS.split('\n'):
+    print("d=",d)
+    (name,port,demo,base) = d.split(',')
+    writefile('/etc/httpd/conf.d/planttracer-{name}.conf', name,
+              HTTPD_TEMPLATE.format(name=name, port=port, demo=demo,base=base))
+    writefile('/etc/systemd/system/planttracer-{name}.service', name,
+              SYSTEMD_TEMPLATE.format(name=name, port=port, demo=demo,base=base))
 
 os.system("sudo systemctl daemon-reload")
