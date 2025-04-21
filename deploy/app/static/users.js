6a855831bbebb0c9cef0ed7b1bb16bd4b5e6747f
Steve Barber <sbarber2@gmail.com>
2025-04-21 14:49:30 -0400

    #687 call API from users.js instead of users.html; bulk_register_setup(); debug; the happy path works now

---
+++ deploy/app/static/users.js
@@ -0,0 +1,38 @@
+"use strict";
+/* jshint esversion: 8 */
+/* global api_key */
+
+////////////////////////////////////////////////////////////////
+// page: /audit
+// This could fill in the table with search keys; right now we just search for everything
+// See https://stackoverflow.com/questions/33682122/datatables-generate-whole-table-from-json
+
+function bulk_register_users() {
+    let formData = new FormData();
+    formData.append("api_key",  api_key); // on the upload form
+    fetch(`${API_BASE}api/get-logs`, { method:"POST", body:formData })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.error!=false){
+                $('#message').html('error: '+data.message);
+                return;
+            }
+            let logs = data.logs;
+            // get the columns
+            var columns = [];
+            for (const key in logs[0]) {
+                columns.push( { data: key, title: key } );
+            }
+            // make the data displayable
+            $('#audit').DataTable( {
+                columns: columns,
+                data: logs
+            });
+        });
+}
+
+$( document ).ready( function() {
+    bulk_register_users();
+});
+
+module.exports = {bulk_register_users}
