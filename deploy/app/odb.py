0b1268bf33ac5ca0d65fb8d67b2ab881d44046de
Simson Garfinkel <simsong@acm.org>
2025-06-15 06:35:22 -0700

    made schema.py a stand-alone type.

--- deploy/app/odb.py
+++ deploy/app/odb.py
@@ -18,7 +18,6 @@ import time
 from collections import defaultdict
 from functools import lru_cache,wraps
 from decimal import Decimal, ROUND_HALF_UP
-from typing import Literal,Optional,Any
 
 from flask import request
 
@@ -26,11 +25,9 @@ import boto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key,Attr
 
-from pydantic import BaseModel,conint,AnyUrl,condecimal,create_model,field_validator,ValidationError
-
 from . import db_object
+from .schema import User, Movie, Trackpoint, validate_movie_field
 from .constants import C
-
 
 # tables
 API_KEYS = 'api_keys'
@@ -80,67 +77,6 @@ PUBLISHED = 'published'
 CREATED = 'created'
 EMAIL = 'email'
 NAME = 'name'
-
-
-class Movie(BaseModel):
-    movie_id: str
-    title: str
-    description: str
-    created_at: conint(gt=0)
-    user_id: str
-    course_id: str
-    status: str
-
-    published: conint(ge=0, le=1)
-    deleted: conint(ge=0, le=1)
-    date_uploaded: Optional[conint(gt=0)] = None
-    orig_movie: Optional[str] = None
-    fps:  Optional[condecimal(max_digits=4, decimal_places=2)] = None
-    width: Optional[conint(ge=0, le=10000)] = None
-    height: Optional[conint(ge=0, le=10000)] = None
-
-    total_frames: Optional[conint(ge=0)] = None
-    total_bytes: Optional[conint(ge=0)] = None
-
-    movie_data_urn: Optional[str] = None
-    movie_zipfile_urn: Optional[str] = None
-
-    last_frame_tracked: Optional[conint(ge=0)] = None
-
-    version: Optional[conint(ge=0)] = None
-
-class Trackpoint(BaseModel):
-    x: Decimal
-    y: Decimal
-    label: str
-    frame_number: Optional[int] = None
-    status: Optional[int] = None
-    err: Optional[Decimal] = None
-
-    @field_validator("x", "y", "err", mode="before")
-    def round_to_one_decimal(cls, v):
-        if v is None:
-            return v
-        d = Decimal(str(v))  # string conversion avoids float issues
-        return d.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
-
-# Function to validate a single prop and value using the Movie schema
-def validate_movie_field(prop: str, value: Any) -> tuple[str, Any]:
-    if prop not in Movie.model_fields:
-        raise AttributeError(f"{prop} is not a valid field of Movie")
-
-    field_type = Movie.model_fields[prop].annotation
-    is_required = Movie.model_fields[prop].is_required
-
-    # Dynamically create a Pydantic model with just this field
-    TempModel = create_model("TempModel", **{prop: (field_type, ... if is_required else None)})
-
-    try:
-        validated = TempModel(**{prop: value})
-        return getattr(validated, prop)
-    except ValidationError as e:
-        raise ValueError(f"Validation error for '{prop}': {e}")
-
 
 
 USER_ID = 'user_id'
