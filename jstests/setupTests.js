ef9d40517861b697e7c62295e382a2bdb3747fbb
Steve Barber <sbarber2@gmail.com>
2025-07-15 07:57:49 -0400

    #591 remove redundant TextEncoder initialization statement

--- jstests/setupTests.js
+++ jstests/setupTests.js
@@ -7,6 +7,3 @@ const { TextEncoder, TextDecoder } = require('util');
 if (typeof global.TextEncoder === 'undefined') {
   global.TextEncoder = TextEncoder;
 }
-if (typeof global.TextDecoder === 'undefined') {
-  global.TextDecoder = TextDecoder;
-}
