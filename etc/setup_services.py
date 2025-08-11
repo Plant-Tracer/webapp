db7c4ff0535d7b2edc9a1712597737333425bb88
Simson Garfinkel <simsong@acm.org>
2025-08-11 01:12:25 +0000

    update

--- etc/setup_services.py
+++ etc/setup_services.py
@@ -3,40 +3,60 @@
 import os
 
 # Define the instances we are creating.
+# name,port,demovar,demoval,base
+DEFS = """
+app,8010,DEMO,OFF,/home/ubuntu/webapp
+demo,8020,DEMO_COURSE_ID,demo,/home/ubuntu/webapp
+dev,8030,DEMO,OFF,/home/ubuntu/dev
+dev-slg,8040,DEMO,OFF,/home/ubuntu/dev-slg
+dev-seb,8050,DEMO,OFF,/home/ubuntu/dev-seb"""
 
 def readfile(fname):
     with open(fname,'r') as f:
         return f.read()
 
 def writefile(fname,name,content):
-    with open(fname.format(name=name), 'w') as f:
+    path = fname.format(name=name)
+    print("Creating",path)
+    with open(path, 'w') as f:
         f.write(content)
 
-HTTPD_TEMPLATE=readfile('planttracer-template.conf')
-SYSTEMD_TEMPLATE=readfile('planttracer-template.service')
 
-# name,port,demovar,demoval,base
-DEFS = """app,8010,DEMO,OFF,/home/ubuntu/webapp
-demo,8020,DEMO_COURSE_ID,demo,/home/ubuntu/webapp
-dev,8030,DEMO,OFF,/home/ubuntu/dev
-dev-slg,8040,DEMO,OFF,/home/ubuntu/dev-slg
-dev-seb,8050,DEMO,OFF,/home/ubuntu/dev-seb
-"""
+if __name__=="__main__":
+    NGINX_TEMPLATE=readfile('planttracer-nginx-template.conf')
+    SERVICE_TEMPLATE=readfile('planttracer-template.service')
 
+    for d in DEFS.strip().split('\n'):
+        try:
+            (name,port,demovar,demoval,base) = d.split(',')
+        except ValueError:
+            print(f"Error in d='{d}'\n")
+            continue
 
-for d in DEFS.split('\n'):
-    print("d=",d)
-    (name,port,demo,base) = d.split(',')
-    writefile('/etc/httpd/conf.d/planttracer-{name}.conf', name,
-              HTTPD_TEMPLATE.format(name=name, port=port, demo=demo,base=base))
-    writefile('/etc/systemd/system/planttracer-{name}.service', name,
-              SYSTEMD_TEMPLATE.format(name=name, port=port, demo=demo,base=base))
+        # "{" is significant in ngnix templates.
+        # Right now we are doing this the ugly way
+        temp = NGINX_TEMPLATE
+        temp=temp.replace("{name}",name)
+        temp=temp.replace("{port}",port)
+        temp=temp.replace("{demovar}",demovar)
+        temp=temp.replace("{demoval}",demoval)
+        temp=temp.replace("{base}",base)
 
-os.system("sudo systemctl daemon-reload")
+        writefile('/etc/nginx/sites-available/{name}.planttracer.com', name, temp)
 
-for d in DEFS.split('\n'):
-    (name,port,demo,base) = d.split(',')
-    service = f'planttracer-{name}'
-    os.system(f"sudo systemctl enable {service}")
-    os.system(f"sudo systemctl start {service}")
-    os.system(f"sudo systemctl restart {service}")
+        writefile('/etc/systemd/system/planttracer-{name}.service', name,
+                  SERVICE_TEMPLATE.format(name=name, port=port, demovar=demovar, demoval=demoval,base=base))
+        symlink_path   = f"/etc/nginx/sites-enabled/{name}.planttracer.com"
+        symlink_target = f"../sites-available/{name}.planttracer.com"
+        if not os.path.exists(symlink_path):
+            os.symlink(symlink_target, symlink_path)
+
+    # Always reload the daemons when the service files are modified
+    os.system("sudo systemctl daemon-reload")
+
+    for d in DEFS.strip().split('\n'):
+        (name,port,demovar,demoval,base) = d.split(',')
+        service = f'planttracer-{name}'
+        os.system(f"sudo systemctl enable {service}")
+        os.system(f"sudo systemctl start {service}")
+        os.system(f"sudo systemctl restart {service}")
