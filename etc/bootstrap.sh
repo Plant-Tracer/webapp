8c79a31d67f40425d178953137604ebab562cdd2
Simson Garfinkel <simsong@acm.org>
2025-12-31 14:54:53 -0500

    aws startup works

--- etc/bootstrap.sh
+++ etc/bootstrap.sh
@@ -1,1 +1,9 @@
+"""
+to-do:
+Reliability (EC2 bootstrap)
+Make the app a systemd service instead of exec "${GitRunPath}" in user-data
+User-data is one-shot; you’ll want restart-on-failure and start-on-boot semantics
+In user-data, write a unit file and systemctl enable --now yourservice. Keep the git checkout/update logic, but run it from a controlled service/script.
+"""
+
 echo Hello World.
