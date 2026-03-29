64b4a12cc01a9423dfd8a959981eac7a66a1ab62
Steve Barber <sbarber2@gmail.com>
2026-03-29 18:17:41 -0400

    Fixes #879 check if rotation value is a string

--- src/app/static/canvas_tracer_controller.js
+++ src/app/static/canvas_tracer_controller.js
@@ -584,7 +584,10 @@ class TracerController extends MovieController {
         // Rotate: server clears tracking, updates rotation_steps, triggers Lambda. Reload when done.
         this.rotate_button.prop(DISABLED, true);
         $('#status-big').html(`Asking server to rotate movie 90º clockwise. Please stand by...`);
-        this.movie_rotation = parseInt(this.move_rotation, 10) + 90;
+        if (typeof this.move_rotation === 'string') {
+            this.movie_rotation = parseInt(this.move_rotation, 10)
+        }
+        this.movie_rotation =  + 90;
         const params = {
           api_key: this.api_key,
           movie_id: this.movie_id,
